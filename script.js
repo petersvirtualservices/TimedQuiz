25420260155e1abc2026f3a8a60c0997c3fa1e23
Charlene Peters <charlene@peters_services.com>
2020-06-27 17:38:13 -0500

    Updated files for design

---
+++ script.js
@@ -0,0 +1,5 @@
+function contextMenuListener(el) {
+    el.addEventListener( "contextmenu", function(e) {
+      alert("Listen here, you little cheater.  I, too, have looked at the source code to find the answer.  You are only hurting yourself because if you do not know the answer by heart, you will probably lose to someone who does.")
+    });
+  }
