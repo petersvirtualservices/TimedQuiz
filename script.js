5d80a72650934cdbfef96955f4b275e09b626201
Charlene Peters <charlene@peters_services.com>
2020-06-28 21:20:39 -0500

    Adding JS updated files

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-function contextMenuListener(el) {
+/* function contextMenuListener(el) {
     el.addEventListener( "contextmenu", function(e) {
       alert("Listen here, you little cheater.  I, too, have looked at the source code to find the answer.  You are only hurting yourself because if you do not know the answer by heart, you will probably lose to someone who does.")
     });
+  } */
+
+//  ************* QUESTION STUFF ********************
+
+// .innerHTML to change questions
+
+/* var question = document.getElementById("question");
+var correct = document.getElementById("right");
+var wrong = document.getElemntbyId("wrong"); */
+
+// ************* TIMER STUFF ********************
+// Need to add 'Adding 3 seconds' for wrong answers
+
+function timeLeft() {
+  var movementPlaceholder = document.getElementById("Progress");
+  var movement = 0;
+  var final = setInterval(timeToMove, 600);
+  function timeToMove() {
+    if (movement >= 100) {
+      clearInterval(final);
+    } else {
+      movement++;
+      movementPlaceholder.style.width = movement + '%';
+    }
   }
+}
