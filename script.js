4b5228f44de5f00d05eb2697455cba4421c90b84
petersvirtualservices <47577984+petersvirtualservices@users.noreply.github.com>
2020-07-04 08:14:48 -0500

    Fixed Question

--- script.js
+++ script.js
@@ -7,20 +7,53 @@ function beginGame() {
 }
 //  ************* QUESTION STUFF ********************
 
+
+
+
 /*  function questionChange (question, right, wrong1, wrong2, wrong3) {
   document.querySelector(".change").addEventListener("click", questionChange());
-  // event.preventDefault() ???
   var question = h2.textContent = questionChange[question];
   var answers = button.textContent = questionChange[right];
   var answers = button.textContent = questionChange[wrong1];
   var answers = button.textContent = questionChange[wrong2];
   var answers = button.textContent = questionChange[wrong3];
+}
+  */
 
-  //create random function for math.random for wrong answers
-} */
+var questions = [
+  { question: "What is a typical abbreviation for Javascript?", answers: ["JS", "Java", "JSS", "Java1"], correctAnswer: 0 },
+];
 
-questionChange("What is a typical abbreviation for Javascript?", "JS", "Java", "JSS", "Java1");
-questionChange("What is the foundational code for a website?", "HTML", "Java",  "Javascript", "CSS");
+// document.getElementsByClassName('change')[0].addEventListener('click', beginGame);
+
+var currentQuestionIndex = 0;
+
+function questionChange() {
+  var changeQuestion = document.getElementsByClassName("change")[0];
+  
+  changeQuestion.innerHTML = '';
+
+  var pInnerText = document.createElement("p");
+  pInnerText.innerText = questions[currentQuestionIndex].question;
+  document.getElementsByTagName("p")[0].appendChild(pInnerText);
+
+  for (var i = 0; i < questions[currentQuestionIndex].answers.length; i++) {
+   var ButtonCreation =  document.createElement("button");
+   ButtonCreation.innerText = questions[currentQuestionIndex].answers[i];
+
+    document.createElement("button").addEventListener("click", function () {
+      if (currentQuestionIndex != questions.length - 1) {
+        currentQuestionIndex += 1;
+        startQuestion();
+      } else {
+
+      }
+      document.getElementById("change").appendChild(button);
+    });
+  }
+}
+//Do for h2
+/* questionChange("What is the foundational code for a website?", "HTML", "Java",  "Javascript", "CSS");
 questionChange("What was Javascript's original name?",  "LiveScript", "Netscape",  "Microsoft", "Juno");
 questionChange("What does console.log() do?",  "It prints the result to the console log.", "It creates code for the site.",  "It does not do anything.", "It creates images.");
 questionChange("What is the right way to declare a variable?",  "var name = 'Whatever'", "name = Whatever",  "Whatver", "Smile.");
@@ -38,7 +71,7 @@ questionChange("What element is used to create an ordered list?", "<ol>",  "<i>"
 questionChange("What is the purpose of the <iframe> element?", "To display a media or webpage within a webpage.", "To load a page.",  "To create a DocType element.", "To allow for elements to be written.");
 questionChange("What is the purpose of the <alt> element?", "To provide text if the image cannot be displayed.", "To create a hyperlink.",  "To resize the image.", "To create a gif from the image.");
 questionChange("SVG used what code?",  "CSS", "HTML",  "Java", "JavaScript");
-questionChange("What is the purpose of Javascript?",  "To provide functionality to a website.", "To offer a base for a website.",  "To add style to a website.", "It does not have a purpose.");
+questionChange("What is the purpose of Javascript?",  "To provide functionality to a website.", "To offer a base for a website.",  "To add style to a website.", "It does not have a purpose."); */
 
 
 // ************* TIMER STUFF ********************
@@ -52,16 +85,16 @@ function timeLeft() {
   function timeToMove() {
     if (movement >= 100) {
       clearInterval(final);
-    } 
-    else{
+    }
+    else {
       movement++;
       movementPlaceholder.style.width = movement + '%';
     }
-    if(questionsRight === 10 && movement >= 100){
+    if (questionsRight === 10 && movement >= 100) {
       window.location.href = "winner.html";
     }
-    else if(questionsRight < 10 && movement >= 100){
-      window.location.href ="loser.html";
+    else if (questionsRight < 10 && movement >= 100) {
+      window.location.href = "loser.html";
     }
 
   }
