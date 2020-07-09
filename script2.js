c63b67536c9b4e7e68a904e5745b0481b5be418a
petersvirtualservices <47577984+petersvirtualservices@users.noreply.github.com>
2020-07-09 17:00:50 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -2,13 +2,9 @@ var nameElem = document.querySelector('#name');
 var buttonElem = document.querySelector('#submit');
 var tableNameElem = document.querySelector('#winnerNameHere');
 
-// listen for click events on the submit button
 buttonElem.addEventListener('click', function() {
-// store the value of the user-specified name
 var name = nameElem.value;
-// store the user name in local storage
 localStorage.setItem('name', name);
-// set the user name in the table
 tableNameElem.innerHTML = name;
 })
 
